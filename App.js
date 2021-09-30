dc9c9b0c9f36e9729c4c64590132f5dd4773fb2e
signey173607 <80842837+signey173607@users.noreply.github.com>
2021-09-30 03:04:20 -0400

    2

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <Main />
-);
+    return (
+        <Main />
+    );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
