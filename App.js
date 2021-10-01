ec914af7e580a686a41d862dca1e15d40e0c0843
signey173607 <80842837+signey173607@users.noreply.github.com>
2021-10-01 02:43:07 -0400

    222

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
@@ -10,11 +8,11 @@ export default function App() {
 );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
