a5a275fce435056a93df171dd3a0ad5a137c63a7
signey173607 <80842837+signey173607@users.noreply.github.com>
2021-10-02 21:57:43 -0400

    task 1

---
+++ components/AboutComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+
+class About extends Component {
+
+    static navigationOptions ={
+        title: 'About Us'
+    }
+    render() {
+        return (
+            <ScrollView>
+
+            </ScrollView>
+        );
+    }
+}
+
+export default About;
