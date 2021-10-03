f4ccdf27f4454e135c2692b516dfd1215786395c
signey173607 <80842837+signey173607@users.noreply.github.com>
2021-10-03 03:36:58 -0400

    all done w1

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, Text, View } from "react-native-gesture-handler";
+import { Card, ListItem } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
+import { FlatList } from 'react-native';
 
+function Mission({item}){
+    if(item) {
+        return (
+            <Card title='Our Mission'>
+                <Text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        }
+    }
 
     static navigationOptions ={
         title: 'About Us'
     }
     render() {
+        const renderPartners = ({item}) => {
+            return ( 
+                <ListItem
+                    title= {item.name}
+                    subtitle= {item.description}
+                    leftAvatar= {{source: require('./images/bootstrap-logo.png')}}
+                />
+            );
+        }
         return (
             <ScrollView>
-
+                <Mission
+                    item={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+                <Card title='Community Partners'>
+                    <FlatList
+                    data={this.state.partners}
+                    renderItem={renderPartners}
+                    keyExtractor={item => item.id.toString()}
+                />
+                </Card>
             </ScrollView>
+            
         );
     }
 }
